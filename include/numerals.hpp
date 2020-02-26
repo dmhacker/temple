c111364672801c260f315116934e82a0ee3ec508
David Hacker <dmhacker@protonmail.com>
2020-02-26 15:02:37 -0800

    Add multiplication operation to build 6, 7

--- include/numerals.hpp
+++ include/numerals.hpp
@@ -1,7 +1,11 @@
 #ifndef TEMPLE_TEMPLE_HPP
 #define TEMPLE_TEMPLE_HPP
 
+#include <cstdint>
+
 namespace temple {
+
+using int_ = uint32_t;
 
 namespace detail {
     template <template <class> class f, class x>
@@ -10,8 +14,8 @@ namespace detail {
     template <template <class> class f, class x>
     struct _church1;
 
-    template <template <class> class f, class x>
-    struct _church2;
+    template <int_ k, template <class> class f, class x>
+    struct _churchk;
 
     template <template <template <class> class, class> class n, template <class> class f, class x>
     struct _inc;
@@ -19,11 +23,14 @@ namespace detail {
     template <template <template <class> class, class> class m, template <template <class> class, class> class n, template <class> class f, class x>
     struct _add;
 
+    template <template <template <class> class, class> class m, template <template <class> class, class> class n, template <class> class f, class x>
+    struct _mul;
+
     template <template <template <template <class> class, class> class, template <class> class, class> class unary,
         template <template <class> class, class> class n>
     struct _apply_unary {
         template <template <class> class f, class x>
-        using church = unary<n, f, x>;
+        using type = unary<n, f, x>;
     };
 
     template <template <template <template <class> class, class> class, template <template <class> class, class> class, template <class> class, class> class binary,
@@ -31,7 +38,7 @@ namespace detail {
         template <template <class> class, class> class n>
     struct _apply_binary {
         template <template <class> class f, class x>
-        using church = binary<m, n, f, x>;
+        using type = binary<m, n, f, x>;
     };
 }
 
@@ -41,14 +48,17 @@ using church0 = typename detail::_church0<f, x>::type;
 template <template <class> class f, class x>
 using church1 = typename detail::_church1<f, x>::type;
 
-template <template <class> class f, class x>
-using church2 = typename detail::_church2<f, x>::type;
+template <int_ k, template <class> class f, class x>
+using churchk = typename detail::_churchk<k, f, x>::type;
 
 template <template <template <class> class, class> class n, template <class> class f, class x>
 using inc = typename detail::_inc<n, f, x>::type;
 
 template <template <template <class> class, class> class m, template <template <class> class, class> class n, template <class> class f, class x>
 using add = typename detail::_add<m, n, f, x>::type;
+
+template <template <template <class> class, class> class m, template <template <class> class, class> class n, template <class> class f, class x>
+using mul = typename detail::_mul<m, n, f, x>::type;
 
 template <template <template <template <class> class, class> class, template <class> class, class> class unary,
     template <template <class> class, class> class n>
@@ -60,13 +70,22 @@ template <template <template <template <class> class, class> class, template <te
 using apply_binary = detail::_apply_binary<binary, m, n>;
 
 template <template <class> class f, class x>
-using church3 = typename apply_unary<inc, church2>::church<f, x>::type;
+using church2 = typename apply_unary<inc, church1>::type<f, x>::type;
 
 template <template <class> class f, class x>
-using church4 = typename apply_binary<add, church2, church2>::church<f, x>::type;
+using church3 = typename apply_unary<inc, church2>::type<f, x>::type;
 
 template <template <class> class f, class x>
-using church5 = typename apply_binary<add, church3, church2>::church<f, x>::type;
+using church4 = typename apply_binary<add, church2, church2>::type<f, x>::type;
+
+template <template <class> class f, class x>
+using church5 = typename apply_binary<add, church3, church2>::type<f, x>::type;
+
+template <template <class> class f, class x>
+using church6 = typename apply_binary<mul, church3, church2>::type<f, x>::type;
+
+template <template <class> class f, class x>
+using church7 = typename apply_unary<inc, church6>::type<f, x>::type;
 
 namespace detail {
     template <template <class> class f, class x>
@@ -79,9 +98,14 @@ namespace detail {
         using type = f<x>;
     };
 
+    template <int_ k, template <class> class f, class x>
+    struct _churchk {
+        using type = f<typename _churchk<k - 1, f, x>::type>;   
+    };
+
     template <template <class> class f, class x>
-    struct _church2 {
-        using type = f<f<x>>;
+    struct _churchk<0, f, x> {
+        using type = x;
     };
 
     template <template <template <class> class, class> class n, template <class> class f, class x>
@@ -92,6 +116,19 @@ namespace detail {
     template <template <template <class> class, class> class m, template <template <class> class, class> class n, template <class> class f, class x>
     struct _add {
         using type = m<f, n<f, x>>;
+    };
+
+    template <template <template <class> class, class> class m, template <template <class> class, class> class n, template <class> class f, class x>
+    struct _mul {
+        template <class y>
+        struct _nfold {
+            using type = n<f, y>;
+        };
+
+        template <class y>
+        using nfold = typename _nfold<y>::type;
+
+        using type = m<nfold, x>;
     };
 }
 
--- src/tests.cpp
+++ src/tests.cpp
@@ -4,14 +4,26 @@
 
 TEST_CASE("Test cases")
 {
+    // Test church numerals from 0 to 10
     REQUIRE(temple::church0<metal::inc, metal::number<0>>() == 0);
     REQUIRE(temple::church1<metal::inc, metal::number<0>>() == 1);
     REQUIRE(temple::church2<metal::inc, metal::number<0>>() == 2);
     REQUIRE(temple::church3<metal::inc, metal::number<0>>() == 3);
     REQUIRE(temple::church4<metal::inc, metal::number<0>>() == 4);
     REQUIRE(temple::church5<metal::inc, metal::number<0>>() == 5);
-    REQUIRE(metal::same<temple::apply_binary<temple::add, temple::church3, temple::church4>::church<metal::inc, metal::number<0>>, 
-            temple::apply_binary<temple::add, temple::church4, temple::church3>::church<metal::inc, metal::number<0>>>());
-    REQUIRE(metal::same<temple::apply_unary<temple::inc, temple::church4>::church<metal::inc, metal::number<0>>, 
+    REQUIRE(temple::church6<metal::inc, metal::number<0>>() == 6);
+    REQUIRE(temple::church7<metal::inc, metal::number<0>>() == 7);
+    REQUIRE(temple::church4<metal::not_, metal::false_>() == false);
+    REQUIRE(temple::church5<metal::not_, metal::false_>() == true);
+
+    // Test arbitrary church numerals (n specified as integer literal)
+    REQUIRE(temple::churchk<10000, metal::inc, metal::number<0>>() == 10000);
+
+    // Test addition operation
+    REQUIRE(metal::same<temple::apply_binary<temple::add, temple::church3, temple::church4>::type<metal::inc, metal::number<0>>, 
+            temple::apply_binary<temple::add, temple::church4, temple::church3>::type<metal::inc, metal::number<0>>>());
+
+    // Test increment operation
+    REQUIRE(metal::same<temple::apply_unary<temple::inc, temple::church4>::type<metal::inc, metal::number<0>>, 
             temple::church5<metal::inc, metal::number<0>>>());
 }
