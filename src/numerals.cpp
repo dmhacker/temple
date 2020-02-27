0893fc50634a0f63bcb1eff09347fad80d8f5c63
David Hacker <dmhacker@protonmail.com>
2020-02-26 21:17:58 -0800

    Add parity tests for church numerals

--- src/numerals.cpp
+++ src/numerals.cpp
@@ -3,9 +3,9 @@
 #include <temple/numerals.hpp>
 
 template <class n>
-using ftst = metal::inc<n>;
+using ITER = metal::add<n, metal::number<2>>;
 
-using xtst = metal::number<0>;
+using BASE = metal::number<0>;
 
 template <template <class> class f, class x>
 using church100 = temple::church<100, f, x>;
@@ -13,85 +13,101 @@ using church100 = temple::church<100, f, x>;
 template <template <class> class f, class x>
 using church99 = temple::church<99, f, x>;
 
-TEST_CASE("Test cases")
+template <template <template <class> class, class> class n>
+using is_even = n<metal::not_, metal::true_>;
+
+template <template <template <class> class, class> class n>
+using is_odd = n<metal::not_, metal::false_>;
+
+TEST_CASE("Test cases for Church numerals")
 {
     // Test church numerals from 0 to 10
-    REQUIRE(temple::church0<ftst, xtst>() == 0);
-    REQUIRE(temple::church1<ftst, xtst>() == 1);
-    REQUIRE(temple::church2<ftst, xtst>() == 2);
-    REQUIRE(temple::church3<ftst, xtst>() == 3);
-    REQUIRE(temple::church4<ftst, xtst>() == 4);
-    REQUIRE(temple::church5<ftst, xtst>() == 5);
-    REQUIRE(temple::church6<ftst, xtst>() == 6);
-    REQUIRE(temple::church7<ftst, xtst>() == 7);
-    REQUIRE(temple::church8<ftst, xtst>() == 8);
-    REQUIRE(temple::church9<ftst, xtst>() == 9);
+    REQUIRE(temple::church0<ITER, BASE>() == 0);
+    REQUIRE(temple::church1<ITER, BASE>() == 2);
+    REQUIRE(temple::church2<ITER, BASE>() == 4);
+    REQUIRE(temple::church3<ITER, BASE>() == 6);
+    REQUIRE(temple::church4<ITER, BASE>() == 8);
+    REQUIRE(temple::church5<ITER, BASE>() == 10);
+    REQUIRE(temple::church6<ITER, BASE>() == 12);
+    REQUIRE(temple::church7<ITER, BASE>() == 14);
+    REQUIRE(temple::church8<ITER, BASE>() == 16);
+    REQUIRE(temple::church9<ITER, BASE>() == 18);
 
     // Test arbitrary church numerals (n specified as integer literal)
-    REQUIRE(church99<metal::inc, metal::number<0>>() == 99);
-    REQUIRE(church100<metal::inc, metal::number<0>>() == 100);
+    REQUIRE(church99<ITER, BASE>() == 198);
+    REQUIRE(church100<ITER, BASE>() == 200);
+
+    // Test parity of numerals
+    REQUIRE(is_odd<temple::church1>());
+    REQUIRE(is_even<temple::church2>());
+    REQUIRE(is_odd<temple::church3>());
+    REQUIRE(is_even<temple::church4>());
+    REQUIRE(is_odd<temple::church5>());
+    REQUIRE(is_even<temple::church6>());
+    REQUIRE(is_odd<church99>());
+    REQUIRE(is_even<church100>());
 
     // Test increment operation
-    REQUIRE(metal::same<temple::inc<temple::church0, ftst, xtst>,
-        temple::church1<ftst, xtst>>());
-    REQUIRE(metal::same<temple::inc<temple::church1, ftst, xtst>,
-        temple::church2<ftst, xtst>>());
-    REQUIRE(metal::same<temple::inc<temple::church4, ftst, xtst>,
-        temple::church5<ftst, xtst>>());
-    REQUIRE(metal::same<temple::inc<church99, ftst, xtst>,
-        church100<ftst, xtst>>());
+    REQUIRE(metal::same<temple::inc<temple::church0, ITER, BASE>,
+        temple::church1<ITER, BASE>>());
+    REQUIRE(metal::same<temple::inc<temple::church1, ITER, BASE>,
+        temple::church2<ITER, BASE>>());
+    REQUIRE(metal::same<temple::inc<temple::church4, ITER, BASE>,
+        temple::church5<ITER, BASE>>());
+    REQUIRE(metal::same<temple::inc<church99, ITER, BASE>,
+        church100<ITER, BASE>>());
 
     // Test decrement operation
-    REQUIRE(metal::same<temple::dec<temple::church0, ftst, xtst>,
-        temple::church0<ftst, xtst>>());
-    REQUIRE(metal::same<temple::dec<temple::church1, ftst, xtst>,
-        temple::church0<ftst, xtst>>());
-    REQUIRE(metal::same<temple::dec<temple::church2, ftst, xtst>,
-        temple::church1<ftst, xtst>>());
-    REQUIRE(metal::same<temple::dec<church100, ftst, xtst>,
-        church99<ftst, xtst>>());
+    REQUIRE(metal::same<temple::dec<temple::church0, ITER, BASE>,
+        temple::church0<ITER, BASE>>());
+    REQUIRE(metal::same<temple::dec<temple::church1, ITER, BASE>,
+        temple::church0<ITER, BASE>>());
+    REQUIRE(metal::same<temple::dec<temple::church2, ITER, BASE>,
+        temple::church1<ITER, BASE>>());
+    REQUIRE(metal::same<temple::dec<church100, ITER, BASE>,
+        church99<ITER, BASE>>());
 
     // Test addition operation
-    REQUIRE(metal::same<temple::add<temple::church3, temple::church4, ftst, xtst>,
-        temple::church7<ftst, xtst>>());
-    REQUIRE(metal::same<temple::add<temple::church3, temple::church4, ftst, xtst>,
-        temple::add<temple::church4, temple::church3, ftst, xtst>>());
-    REQUIRE(metal::same<temple::add<temple::church0, temple::church0, ftst, xtst>,
-        temple::church0<ftst, xtst>>());
-    REQUIRE(metal::same<temple::add<temple::church4, temple::church4, ftst, xtst>,
-        temple::church8<ftst, xtst>>());
-    REQUIRE(metal::same<temple::add<temple::church3, temple::church6, ftst, xtst>,
-        temple::church9<ftst, xtst>>());
+    REQUIRE(metal::same<temple::add<temple::church3, temple::church4, ITER, BASE>,
+        temple::church7<ITER, BASE>>());
+    REQUIRE(metal::same<temple::add<temple::church3, temple::church4, ITER, BASE>,
+        temple::add<temple::church4, temple::church3, ITER, BASE>>());
+    REQUIRE(metal::same<temple::add<temple::church0, temple::church0, ITER, BASE>,
+        temple::church0<ITER, BASE>>());
+    REQUIRE(metal::same<temple::add<temple::church4, temple::church4, ITER, BASE>,
+        temple::church8<ITER, BASE>>());
+    REQUIRE(metal::same<temple::add<temple::church3, temple::church6, ITER, BASE>,
+        temple::church9<ITER, BASE>>());
 
     // Test multiplication operation
-    REQUIRE(metal::same<temple::mul<temple::church2, temple::church4, ftst, xtst>,
-        temple::church8<ftst, xtst>>());
-    REQUIRE(metal::same<temple::mul<temple::church4, temple::church2, ftst, xtst>,
-        temple::church8<ftst, xtst>>());
-    REQUIRE(metal::same<temple::mul<temple::church1, temple::church6, ftst, xtst>,
-        temple::church6<ftst, xtst>>());
-    REQUIRE(metal::same<temple::mul<temple::church6, temple::church1, ftst, xtst>,
-        temple::church6<ftst, xtst>>());
-    REQUIRE(metal::same<temple::mul<temple::church0, temple::church8, ftst, xtst>,
-        temple::church0<ftst, xtst>>());
-    REQUIRE(metal::same<temple::mul<temple::church8, temple::church0, ftst, xtst>,
-        temple::church0<ftst, xtst>>());
+    REQUIRE(metal::same<temple::mul<temple::church2, temple::church4, ITER, BASE>,
+        temple::church8<ITER, BASE>>());
+    REQUIRE(metal::same<temple::mul<temple::church4, temple::church2, ITER, BASE>,
+        temple::church8<ITER, BASE>>());
+    REQUIRE(metal::same<temple::mul<temple::church1, temple::church6, ITER, BASE>,
+        temple::church6<ITER, BASE>>());
+    REQUIRE(metal::same<temple::mul<temple::church6, temple::church1, ITER, BASE>,
+        temple::church6<ITER, BASE>>());
+    REQUIRE(metal::same<temple::mul<temple::church0, temple::church8, ITER, BASE>,
+        temple::church0<ITER, BASE>>());
+    REQUIRE(metal::same<temple::mul<temple::church8, temple::church0, ITER, BASE>,
+        temple::church0<ITER, BASE>>());
 
     // Test subtraction operation
-    REQUIRE(metal::same<temple::sub<temple::church0, temple::church0, ftst, xtst>,
-        temple::church0<ftst, xtst>>());
-    REQUIRE(metal::same<temple::sub<temple::church0, temple::church5, ftst, xtst>,
-        temple::church0<ftst, xtst>>());
-    REQUIRE(metal::same<temple::sub<temple::church4, temple::church5, ftst, xtst>,
-        temple::church0<ftst, xtst>>());
-    REQUIRE(metal::same<temple::sub<temple::church5, temple::church5, ftst, xtst>,
-        temple::church0<ftst, xtst>>());
-    REQUIRE(metal::same<temple::sub<church100, church99, ftst, xtst>,
-        temple::church1<ftst, xtst>>());
-    REQUIRE(metal::same<temple::sub<church100, temple::church1, ftst, xtst>,
-        church99<ftst, xtst>>());
-    REQUIRE(metal::same<temple::sub<temple::church7, temple::church2, ftst, xtst>,
-        temple::church5<ftst, xtst>>());
-    REQUIRE(metal::same<temple::sub<temple::church4, temple::church1, ftst, xtst>,
-        temple::dec<temple::church4, ftst, xtst>>());
+    REQUIRE(metal::same<temple::sub<temple::church0, temple::church0, ITER, BASE>,
+        temple::church0<ITER, BASE>>());
+    REQUIRE(metal::same<temple::sub<temple::church0, temple::church5, ITER, BASE>,
+        temple::church0<ITER, BASE>>());
+    REQUIRE(metal::same<temple::sub<temple::church4, temple::church5, ITER, BASE>,
+        temple::church0<ITER, BASE>>());
+    REQUIRE(metal::same<temple::sub<temple::church5, temple::church5, ITER, BASE>,
+        temple::church0<ITER, BASE>>());
+    REQUIRE(metal::same<temple::sub<church100, church99, ITER, BASE>,
+        temple::church1<ITER, BASE>>());
+    REQUIRE(metal::same<temple::sub<church100, temple::church1, ITER, BASE>,
+        church99<ITER, BASE>>());
+    REQUIRE(metal::same<temple::sub<temple::church7, temple::church2, ITER, BASE>,
+        temple::church5<ITER, BASE>>());
+    REQUIRE(metal::same<temple::sub<temple::church4, temple::church1, ITER, BASE>,
+        temple::dec<temple::church4, ITER, BASE>>());
 }
